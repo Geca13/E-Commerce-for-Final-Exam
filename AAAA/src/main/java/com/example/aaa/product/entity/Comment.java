2902a05ea24d186481c4d55b30603a9d12e74ce7
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-12 23:54:59 +0100

    Update Comment.java

--- AAAA/src/main/java/com/example/aaa/product/entity/Comment.java
+++ AAAA/src/main/java/com/example/aaa/product/entity/Comment.java
@@ -1,7 +1,6 @@
 package com.example.aaa.product.entity;
 
 import java.time.LocalDateTime;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,9 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import com.example.aaa.users.entity.Users;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
