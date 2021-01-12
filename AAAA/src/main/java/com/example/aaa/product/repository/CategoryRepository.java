80f4efe63436805e562a5672ea36cb8b0b18cd07
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 00:03:33 +0100

    Update CategoryRepository.java

--- AAAA/src/main/java/com/example/aaa/product/repository/CategoryRepository.java
+++ AAAA/src/main/java/com/example/aaa/product/repository/CategoryRepository.java
@@ -1,12 +1,10 @@
 package com.example.aaa.product.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.example.aaa.product.entity.Category;
 
 @Repository
@@ -19,6 +17,4 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	@Query(value="select * from Category p where p.product_category like :word ", nativeQuery = true)
 	List<Category> findByWord(@Param("word") String word);
 	
-	
-
 }
