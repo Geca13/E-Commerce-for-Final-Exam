849b80f6f4a611d3a28d14488570959e5427f6b3
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 00:06:57 +0100

    Update ProductRepository.java

--- AAAA/src/main/java/com/example/aaa/product/repository/ProductRepository.java
+++ AAAA/src/main/java/com/example/aaa/product/repository/ProductRepository.java
@@ -1,61 +1,37 @@
 package com.example.aaa.product.repository;
 
-
-import java.time.LocalDateTime;
 import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.example.aaa.product.entity.Category;
-import com.example.aaa.product.entity.Manufacturer;
 import com.example.aaa.product.entity.Product;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	 
+        Boolean existsByKeyword(String keyword);
 	
-	// sto pravime ako imaat isto iminja???
-	
-	Boolean existsByProductName(String productName);
-	
-    Product findByProductName(String productName);
-	
-    
-    Boolean existsByKeyword(String keyword);
-	
- //   Product findByKeyword(String keyword);
-    
-    
-    
-    
- 
-    
-    
+        Product findByKeyword(String keyword);
 
-	@Query("SELECT DISTINCT product FROM Product product WHERE product.keyword LIKE :keyword%")
+	@Query("SELECT p FROM Product p WHERE CONCAT(p.productName, ' ', p.productPrice, ' ', p.keyword) LIKE %?1%")
 	@Transactional(readOnly = true)
-	List<Product> findByKeyword(String keyword);
+	Page<Product> findBySearch(String search,Pageable pageable );
 	
-//	@Query("SELECT DISTINCT product FROM Product product WHERE product.country.countryName LIKE :productOrigin%")
-//	@Transactional(readOnly = true)
-//	List<Product>byOrigin(String countryName);
-
+	Page<Product>findAllProductByCategoryId(Integer pid, Pageable pageable);
 	
+	Page<Product>findAllProductByManufacturerId(Integer mid, Pageable pageable);
 	
+	Page<Product>findAllProductByCountryId(Integer cid, Pageable pageable);
 	
+	Page<Product>findAllProductByStoreId(Integer sid, Pageable pageable);
 	
-	Page<Product> findByCategory(Category category,Pageable pageable );
+        List<Product> findAllProductById(Integer id, Pageable pageable);
 	
-	List<Product> findByManufacturer(Manufacturer manufacturer);
-	
-	
-	
-	
-
+	@Query("select u from Product u where u.availableQty > 0")
+	Page<Product> findAllProductByAvailableQty(Pageable pageable);
+		
 }
