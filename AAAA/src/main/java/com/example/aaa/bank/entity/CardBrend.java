10ca5f77d099cf8b773acf22fccd03d2bc15c89f
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-12 23:34:59 +0100

    Update CardBrend.java

--- AAAA/src/main/java/com/example/aaa/bank/entity/CardBrend.java
+++ AAAA/src/main/java/com/example/aaa/bank/entity/CardBrend.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +17,7 @@ import lombok.NoArgsConstructor;
 public class CardBrend {
 	
 	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String brend;
