dc6c5a3369bdb6d7ca65323fa5e165f114b6b590
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-12 23:59:18 +0100

    Update Store.java

--- AAAA/src/main/java/com/example/aaa/product/entity/Store.java
+++ AAAA/src/main/java/com/example/aaa/product/entity/Store.java
@@ -1,16 +1,16 @@
 package com.example.aaa.product.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
 import com.example.aaa.bank.entity.Bank;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -34,8 +34,12 @@ public class Store {
 	@JoinColumn(referencedColumnName = "id")
 	private Bank bank;
 	
-    private String accountNumber;
+	private String accountNumber;
 	
 	private Double balance;
+	
+	@Lob
+	@Column
+	private String image;
 
 }
