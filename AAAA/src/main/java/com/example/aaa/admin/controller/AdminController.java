dbc68736029c4278ad5193cb4b8787bb2942cee6
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-12 20:10:25 +0100

    Update AdminController.java

--- AAAA/src/main/java/com/example/aaa/admin/controller/AdminController.java
+++ AAAA/src/main/java/com/example/aaa/admin/controller/AdminController.java
@@ -1,10 +1,14 @@
 package com.example.aaa.admin.controller;
 
-
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
 import java.util.List;
-
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -46,6 +50,8 @@ public class AdminController {
 	PaymentRepository paymentRepository;
 	@Autowired
 	CompanyRepository companyRepository;
+	@Autowired
+	private JavaMailSender sender;
 	
 	@GetMapping("/administration")
 	public String administrationPage(Model model,@AuthenticationPrincipal UsersDetails userD) {
@@ -74,7 +80,8 @@ public class AdminController {
 		List<Store> stores = storeRepository.findAll();
 		  Double totalStoresBalance = 0.00;
 		  for (Store store : stores) {
-			  totalStoresBalance = totalStoresBalance +  store.getBalance();
+			  totalStoresBalance = totalStoresBalance + store.getBalance();
+			  storeRepository.save(store);
 		}
 		  model.addAttribute("stores", stores);
 		  model.addAttribute("totalStoresBalance", totalStoresBalance);
@@ -84,19 +91,17 @@ public class AdminController {
 		  
 		  for (Payment payment : payments) {
 			  payedToStores = payedToStores + payment.getSum();
-		  
-			  
-		}
+			       model.addAttribute("payedToStores", payedToStores);
+			  }
 		  
 		  Integer payToStoresTransactions = payments.size();
 		  model.addAttribute("payToStoresTransactions", payToStoresTransactions);
-		  model.addAttribute("payedToStores", payedToStores);
 		  
 		  Company company = companyRepository.findByAccountNumber("0123456789");
 		  Double balance = company.getAccountBalance();
 		  model.addAttribute("balance", balance);
 		
-		return "adminPage";
+		              return "adminPage";
 		
 	    }
 	
@@ -105,23 +110,22 @@ public class AdminController {
 		
 		 orderPages(1, model);
 		 
-		return "orders";
+		        return "orders";
 	}
 	 
 	 @GetMapping("/pG/{pN}")
 	 public String orderPages(@PathVariable("pN")Integer pN, Model model) {
 		 
-		 Integer pS = 2;
+		 Integer pS = 12;
 		 
 		 Page<Order> or = orderService.orders(pN, pS);
 		 List<Order> orders = or.getContent();
 		 
-		   
 		    model.addAttribute("orders", orders);
 		    model.addAttribute("currentPage",pN);
 			model.addAttribute("totalPages", or.getTotalPages());
 			model.addAttribute("totalItems", or.getTotalElements());
-		 return "orders";
+		          return "orders";
 	 }
 	 
 	 @GetMapping("/payments")
@@ -150,10 +154,72 @@ public class AdminController {
 			model.addAttribute("sortDir", sortDir);
 			model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
 			model.addAttribute("payments", payments);
-		 return "payments";
+		            return "payments";
 	 }
 	 
+	 @GetMapping("/users/profile/{id}")
+	 public String seeUserProfile(Model model,@PathVariable("id")Integer id) {
+		 Double totalSpent=0.00;
+		 Users user = userRepository.findById(id).get();
+		 List<Order>orders = orderRepository.findByUser(user);
+		  for (Order order : orders) {
+			  totalSpent = totalSpent+order.getTotal();
+			  model.addAttribute("totalSpent", totalSpent);
+		}
+				model.addAttribute("orders", orders);
+				model.addAttribute("user", user);
+		 
+		 return "adminSeeUserProfile";
+	 }
 	 
-	 
-	 
+	 @GetMapping("/pay/{id}")
+	 public String payToStoresManually(@PathVariable("id")Integer id,Model model) {
+			 Payment payment = new Payment();
+			 Company comp = companyRepository.findByAccountNumber("0123456789");
+			 Store store = storeRepository.findById(id).get();
+			 Company company = companyRepository.findByAccountNumber(store.getAccountNumber());
+			   company.setAccountBalance(company.getAccountBalance() + store.getBalance());
+			   comp.setAccountBalance(comp.getAccountBalance() - store.getBalance());
+			 
+			     payment.setCompany(company);
+				 payment.setSum(store.getBalance());
+				 payment.setLocalDateTime(LocalDateTime.now());
+				 paymentRepository.save(payment);
+				 
+			 String email = payment.getCompany().getEmail();
+			 Double amount = payment.getSum();
+			 LocalDateTime time = payment.getLocalDateTime();
+			 Double balance = payment.getCompany().getAccountBalance();
+				 
+				 try {
+					sendPaymentEmailToStore(email, amount,time,balance);
+				} catch (UnsupportedEncodingException | MessagingException ex) {
+					model.addAttribute("error", ex.getMessage());
+					model.addAttribute("error", "Something bad happened while sending the email.");
+				}
+			   store.setBalance(0.00);
+			         companyRepository.save(company);
+			         companyRepository.save(comp);
+			         storeRepository.save(store);
+			 
+			             return "redirect:/administration";
+			 }
+		 
+	 private void sendPaymentEmailToStore(String email, Double amount,LocalDateTime time, Double balance) throws UnsupportedEncodingException, MessagingException {
+				
+			MimeMessage message = sender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+				
+			   helper.setFrom("Geca@geca.com", "Java imitacija");
+			   helper.setTo(email);
+				
+			String subject = "Payment ";
+			String content =  "You have received a Payment from Dzikle's Express !!! on " + time + 
+						" . The amount that was payed  was  " + amount +"$" + ". The new balance on your account now is: "+ balance + "$" ;
+				
+			   helper.setSubject(subject);
+			   helper.setText(content, true);
+				  sender.send(message);
+	 }	 
 }
+
