28e4e8700cdfe9115f9318a1bdeeb6623bc4eb2e
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-12 23:43:53 +0100

    Update CompanyRepository.java

--- AAAA/src/main/java/com/example/aaa/bank/repository/CompanyRepository.java
+++ AAAA/src/main/java/com/example/aaa/bank/repository/CompanyRepository.java
@@ -2,7 +2,6 @@ package com.example.aaa.bank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.aaa.bank.entity.Company;
 
 @Repository
