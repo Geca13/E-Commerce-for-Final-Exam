55ec5dbe23b253da089089911fba682f58eb7cad
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 00:04:41 +0100

    Update ManufacturerRepository.java

--- AAAA/src/main/java/com/example/aaa/product/repository/ManufacturerRepository.java
+++ AAAA/src/main/java/com/example/aaa/product/repository/ManufacturerRepository.java
@@ -1,15 +1,11 @@
 package com.example.aaa.product.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-
 import com.example.aaa.product.entity.Manufacturer;
-
 
 @Repository
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer> {
@@ -19,6 +15,6 @@ public interface ManufacturerRepository extends JpaRepository<Manufacturer, Inte
     Manufacturer findByManufacturerName(String manufacturerName);
     
     @Query(value="select * from Manufacturer m where p.manufacturer_name like :word ", nativeQuery = true)
-	List<Manufacturer> findByWord(@Param("word") String word);
+    List<Manufacturer> findByWord(@Param("word") String word);
 
 }
