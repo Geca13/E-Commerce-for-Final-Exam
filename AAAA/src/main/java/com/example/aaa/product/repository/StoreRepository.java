687a67403ed8df0c6aac89797be75ceaac765588
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 00:07:57 +0100

    Update StoreRepository.java

--- AAAA/src/main/java/com/example/aaa/product/repository/StoreRepository.java
+++ AAAA/src/main/java/com/example/aaa/product/repository/StoreRepository.java
@@ -2,8 +2,6 @@ package com.example.aaa.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.aaa.bank.entity.Bank;
 import com.example.aaa.product.entity.Store;
 
 @Repository
