1d86d57665d0d37418f0eae3c36fdd6ce4e28b73
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:38:47 +0100

    Update CountryRepository.java

--- AAAA/src/main/java/com/example/aaa/users/repository/CountryRepository.java
+++ AAAA/src/main/java/com/example/aaa/users/repository/CountryRepository.java
@@ -2,8 +2,6 @@ package com.example.aaa.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
 import com.example.aaa.users.entity.Country;
 
 @Repository
@@ -11,6 +9,6 @@ public interface CountryRepository extends JpaRepository<Country, Integer> {
 	
     Country findByCountryName(String countryName);
 	
-	Boolean existsByCountryName(String countryName);
+    Boolean existsByCountryName(String countryName);
 
 }
