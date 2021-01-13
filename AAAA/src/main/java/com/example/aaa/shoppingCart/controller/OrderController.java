3078eda675396c691e7518f72b570b11e891af46
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:07:03 +0100

    Update OrderController.java

--- AAAA/src/main/java/com/example/aaa/shoppingCart/controller/OrderController.java
+++ AAAA/src/main/java/com/example/aaa/shoppingCart/controller/OrderController.java
@@ -1,14 +1,11 @@
 package com.example.aaa.shoppingCart.controller;
 
-
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.query.Param;
@@ -21,10 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import com.example.aaa.bank.entity.CreditCard;
 import com.example.aaa.bank.repository.CreditCardRepository;
-import com.example.aaa.product.controller.ProductController;
 import com.example.aaa.product.entity.Product;
 import com.example.aaa.product.repository.ProductRepository;
 import com.example.aaa.shoppingCart.entity.CartProducts;
@@ -38,7 +32,6 @@ import com.example.aaa.shoppingCart.repository.OrderRepository;
 import com.example.aaa.shoppingCart.repository.ShippingOptionsRepository;
 import com.example.aaa.shoppingCart.repository.ShoppingCartRepository;
 import com.example.aaa.users.entity.Address;
-import com.example.aaa.users.entity.Country;
 import com.example.aaa.users.entity.Users;
 import com.example.aaa.users.repository.AddressRepository;
 import com.example.aaa.users.repository.CountryRepository;
@@ -48,7 +41,6 @@ import com.example.aaa.users.service.UsersDetails;
 @Controller
 public class OrderController {
 
-	
 	@Autowired
 	ProductRepository productRepository;
 	@Autowired
@@ -76,49 +68,44 @@ public class OrderController {
 	String destinationEmail;
 	
 	
-	@GetMapping("/confirmAndPay/{id}")
-	public String confirmAndPayment(@AuthenticationPrincipal UsersDetails userD,Model model, @PathVariable("id") Integer id) {
-		
+	@GetMapping("/confirmAndPay")
+	public String confirmAndPayment(@AuthenticationPrincipal UsersDetails userD,Model model) {
 		
 		String userEmail = userD.getUsername();
         Users user = userRepository.findByEmail(userEmail);
-        ShoppingCart cart = cartRepository.findById(id).get();
+        ShoppingCart cart = user.getCart();
         
         List<ShippingOption> options = soRepository.findAll();
         List<Address> listaddress = new ArrayList<>();
+           
         List<Address> address1 = addressRepository.findAll();
         for (Address address2 : address1) {
 			if(address2.getUser().getId() == user.getId()) {
 				listaddress.add(address2);
 			}
 		}
+        if(listaddress.isEmpty()) {
+     	   model.addAttribute("messageAddress", "It looks like you are going to make your first order, please add a shipping address first");
+     	 }
          model.addAttribute("cart", cart);
-        
-		 model.addAttribute("user", user);
-		
+         model.addAttribute("user", user);
 		 model.addAttribute("options", options);
-		
-	   	 model.addAttribute("listaddress", listaddress);
+		 model.addAttribute("listaddress", listaddress);
 	   	 
-	   	 
-		
-		return "shippmentAndPayment";
+	   	        return "shippmentAndPayment";
 	}
 	
-	@PostMapping("/confirmAndPay/{id}")
-	public String proccessPayment(@AuthenticationPrincipal UsersDetails userD,@PathVariable("id") Integer id, ShoppingCart cc) {
+	@PostMapping("/confirmAndPay")
+	public String proccessPayment(@AuthenticationPrincipal UsersDetails userD, ShoppingCart cc) {
 		
 		String userEmail = userD.getUsername();
         Users user = userRepository.findByEmail(userEmail);
-        ShoppingCart cart = cartRepository.findById(id).get();
+        ShoppingCart cart = user.getCart();
        
+          cart.setAddress(cc.getAddress());
+          cart.setOption(cc.getOption());
         
-        cart.setAddress(cc.getAddress());
-        cart.setOption(cc.getOption());
-        
-        
-        
-        cartRepository.save(cart);
+          cartRepository.save(cart);
         
 		return "redirect:/payment/"+ cart.getId();
 	}
@@ -133,31 +120,35 @@ public class OrderController {
         Users user = userRepository.findByEmail(userEmail);
         ShoppingCart cart = user.getCart();
         
-        order1.setDate(LocalDate.now());
-        order1.setUser(user);
-        order1.setShipping("by "+cart.getOption().getTransporter() +  " in "  + cart.getOption().getDays());
-        order1.setSubtotal(cart.getTotal());
-        order1.setTotal(cart.getTotal()+cart.getOption().getPrice());
-        order1.setAddress(cart.getAddress().getStreetName() + " "+ cart.getAddress().getStreetNumber()+ " "+ cart.getAddress().getCity()+ " "+ cart.getAddress().getZipCode()+ " " + cart.getAddress().getCountry().getCountryName());
-        
-        
+            order1.setDate(LocalDate.now());
+            order1.setUser(user);
+            order1.setShipping("by "+cart.getOption().getTransporter() +  " in "  + cart.getOption().getDays());
+            order1.setSubtotal(cart.getTotal());
+            order1.setTotal(cart.getTotal()+cart.getOption().getPrice());
+            order1.setAddress(cart.getAddress().getStreetName() + " " + cart.getAddress().getStreetNumber()+ " " + cart.getAddress().getCity()+ " " + cart.getAddress().getZipCode()+ " " + cart.getAddress().getCountry().getCountryName());
+       
         List<OrderProducts> products = new ArrayList<>();
-        
         List<CartProducts> cartProducts = user.getCart().getProducts();
             
-        for (CartProducts cartProducts2 : cartProducts) {
-			cartProducts2.getProduct().setAvailableQty(cartProducts2.getProduct().getAvailableQty()-cartProducts2.getQty());
-			
-			     if(cartProducts2.getProduct().getAvailableQty() < cartProducts2.getQty()) {
-			    	 
+            for (CartProducts cartProducts2 : cartProducts) {
+		     if(cartProducts2.getProduct().getAvailableQty() < cartProducts2.getQty()) {
 			    	 return "redirect:/profile/cart?beated";
-			     }
-			
+			      }
+		    cartProducts2.getProduct().setAvailableQty(cartProducts2.getProduct().getAvailableQty() - cartProducts2.getQty());
+			  
 			productRepository.save(cartProducts2.getProduct());
+			
+			if(cartProducts2.getProduct().getAvailableQty()==0) {
+				try {
+					sendProductIsOutOfStockEmailToStore(cartProducts2.getProduct());
+				} catch (UnsupportedEncodingException | MessagingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
 		}
-           
-           
-        for (CartProducts cartProducts2 : cartProducts) {
+            for (CartProducts cartProducts2 : cartProducts) {
         	OrderProducts product = new OrderProducts();
         	product.setPid(cartProducts2.getProduct().getId());
         	product.setName(cartProducts2.getProduct().getProductName());
@@ -175,46 +166,35 @@ public class OrderController {
             orderRepository.save(order1);
         
             for (CartProducts cartProducts2 : cartProducts) {
-			
-        	cartProducts2.setProduct(null);
-        	
+			cartProducts2.setProduct(null);
         	cartPRepository.save(cartProducts2);
         	}
             cartProducts.clear();
        
-       List<CartProducts> cartProduc=  cartPRepository.findAll();
+        List<CartProducts> cartProduc=  cartPRepository.findAll();
        
-       for (CartProducts cartProducts2 : cartProduc) {
-    	   
-    	   if(cartProducts2.getProduct()==null) {
+             for (CartProducts cartProducts2 : cartProduc) {
+    	       if(cartProducts2.getProduct()==null) {
     		   cartPRepository.delete(cartProducts2); 
     	   }
 		}
-        cart.setTotal(null);
-        cart.setAddress(null);
+            cart.setTotal(null);
+            cart.setAddress(null);
         
-        
-        
-          cartRepository.save(cart);
-          userRepository.save(user);
+         cartRepository.save(cart);
+         userRepository.save(user);
           
-          
-          
-          try {
+         try {
 			sendOrderToAdmin(destinationEmail, order1);
 		} catch (UnsupportedEncodingException | MessagingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-		
-		return"redirect:/order/"+order1.getId();
+         
+        return"redirect:/order/"+order1.getId();
 	}
 	
-	
-	
 	private void sendOrderToAdmin(String destinationEmail2, Order order1) throws UnsupportedEncodingException, MessagingException {
-		
 		
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
@@ -230,23 +210,45 @@ public class OrderController {
 		String content =  "New order with Id :" + order1.getId() + " was completed ..."
 				+ "<p> Customer with shipping address: </p>"
 				+"<p>"+ order1.getAddress() + "</p>"
-				+ "<p> ordered these products </p>" 
+				+ "<p> ordered product with </p>" 
 				//+ "<p><b>"+order1.getProducts() + "</b></p>"
-				+ "<p><b>"+order.getPid() + ", "+order.getName()+", "+order.getQty() + "</b></p>"
+				+ "<p><b> id= "+order.getPid() + ", "+order.getName()+",and qty= "+order.getQty() + "</b></p>"
 				+"<p> that needs to be shipped </p>"
-				+"<p>"+ order1.getShipping() + "</p>"
-				+"<h1>jhdhfkjshdjfkdsj</h1>"
-				;
+				+"<p>"+ order1.getShipping() + "</p>";
 		   
 		helper.setSubject(subject);
 		helper.setText(content, true);
 		sender.send(message);
+		
+			}
+		}
+	
+    private void sendProductIsOutOfStockEmailToStore( Product product) throws UnsupportedEncodingException, MessagingException {
+		
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		  helper.setFrom("Geca@geca.com", "Java imitacija");
+		  helper.setTo(product.getStore().getEmail());
+		
+		String subject = "Product is out of Stock";
+		String content =  "On the last order the product "
+				+"<p>"+ product.getProductName() +"</p>"
+						+ "<p>manufactured from:  </p>"
+				+"<p>"+ product.getManufacturer().getManufacturerName() + "</p>"
+				+ "<p>became unavailable , we sold all the quantity you submitted. </p>" 
+				+ "<p>Please contact us if you want to sell more quantities from it on our platform.</p>" 
+				+ "<p>If you do want to keep selling the product please provide the quantity you want to sell and the product id:</p>" + product.getId()
+		        + "<p>Thank you and have a nice day.</p>" ;
+		   
+		  helper.setSubject(subject);
+		  helper.setText(content, true);
+		        sender.send(message);
+		
 			}
 		
-	}
-
-	@GetMapping("/order/{id}")
-	public String getOrder(Model model, @PathVariable("id")Integer id,@AuthenticationPrincipal UsersDetails userD) {
+     @GetMapping("/order/{id}")
+	 public String getOrder(Model model, @PathVariable("id")Integer id,@AuthenticationPrincipal UsersDetails userD) {
 		
 		Order order = orderRepository.findById(id).get();
 		String userEmail = userD.getUsername();
@@ -255,7 +257,6 @@ public class OrderController {
         List<OrderProducts> pro = orderPRepository.findAll();
         for (OrderProducts orderProducts : pro) {
 		    if(order.getProducts().contains(orderProducts ) ) {
-				
 				products.add(orderProducts);
 			}
         }
@@ -263,9 +264,7 @@ public class OrderController {
 		model.addAttribute("order", order);
 		model.addAttribute("user", user);
 		
-	return "confirmOrder";
-		
-	}
-	
- 
-}
+	           return "confirmOrder";
+		}
+	 	
+ }
