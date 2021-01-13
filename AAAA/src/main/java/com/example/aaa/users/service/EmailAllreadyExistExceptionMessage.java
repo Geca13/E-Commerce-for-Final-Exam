03320be52515fb5373aa2abdabb7c6d1d7fdd27c
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:41:34 +0100

    Add files via upload

---
+++ AAAA/src/main/java/com/example/aaa/users/service/EmailAllreadyExistExceptionMessage.java
@@ -0,0 +1,12 @@
+package com.example.aaa.users.service;
+
+public class EmailAllreadyExistExceptionMessage extends Exception {
+
+	public EmailAllreadyExistExceptionMessage(String message) {
+		super(message);
+		
+	}
+
+	
+
+}
