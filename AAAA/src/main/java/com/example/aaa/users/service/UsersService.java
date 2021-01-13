ea42deb5daa0dbd7c0fc83c1405426a262f3fc9e
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:44:25 +0100

    Update UsersService.java

--- AAAA/src/main/java/com/example/aaa/users/service/UsersService.java
+++ AAAA/src/main/java/com/example/aaa/users/service/UsersService.java
@@ -2,9 +2,6 @@ package com.example.aaa.users.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-
-
 import com.example.aaa.users.entity.Users;
 
 public interface UsersService extends UserDetailsService {
