d87ae9a151c8060dd372f5acf5ed36d8b5d8d02c
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:38:12 +0100

    Update AddressRepository.java

--- AAAA/src/main/java/com/example/aaa/users/repository/AddressRepository.java
+++ AAAA/src/main/java/com/example/aaa/users/repository/AddressRepository.java
@@ -2,7 +2,6 @@ package com.example.aaa.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.example.aaa.users.entity.Address;
 
 @Repository
