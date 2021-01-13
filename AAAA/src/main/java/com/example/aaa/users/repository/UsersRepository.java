eedb714d9604e7e338f981367e4a3215f96a0ae3
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:40:30 +0100

    Update UsersRepository.java

--- AAAA/src/main/java/com/example/aaa/users/repository/UsersRepository.java
+++ AAAA/src/main/java/com/example/aaa/users/repository/UsersRepository.java
@@ -1,8 +1,10 @@
 package com.example.aaa.users.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.example.aaa.users.entity.Users;
 
 @Repository
@@ -11,5 +13,8 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
 	Users findByEmail(String email);
 	
 	Users findByToken(String token);
+	
+	@Query("SELECT u FROM Users u WHERE CONCAT(u.firstName, ' ', u.lastName, ' ', u.email) LIKE %?1%")
+	Page<Users> findBySearch(String search, Pageable pageable);
 
 }
