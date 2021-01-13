7e0fe829ad4fdd1849896e9fcd9c390bceeb009e
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:30:22 +0100

    Update ForgotPasswordController.java

--- AAAA/src/main/java/com/example/aaa/users/controller/ForgotPasswordController.java
+++ AAAA/src/main/java/com/example/aaa/users/controller/ForgotPasswordController.java
@@ -1,25 +1,21 @@
 package com.example.aaa.users.controller;
 
 import java.io.UnsupportedEncodingException;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.example.aaa.users.entity.Users;
+import com.example.aaa.users.service.InvalidPasswordException;
 import com.example.aaa.users.service.UserNotFoundException;
 import com.example.aaa.users.service.UsersServiceImpl;
-
 import net.bytebuddy.utility.RandomString;
 
 @Controller
@@ -34,8 +30,7 @@ public class ForgotPasswordController {
 	@GetMapping ("/forgotPassword")
 	public String showForgotPasswordForm(Model model) {
 		
-		
-		return "forgottenPassord";
+		    return "forgottenPassord";
 	}
 	
 	@PostMapping("/forgotPassword")
@@ -47,7 +42,7 @@ public class ForgotPasswordController {
 		try {
 			userService.forgotPassword(token, email);
 			
-			String resetPasswordLink = getSiteUrl(request) + "/newPassword?token=" + token;
+		String resetPasswordLink = getSiteUrl(request) + "/newPassword?token=" + token;
 			
 			sendEmail(email,resetPasswordLink);
 			
@@ -59,7 +54,7 @@ public class ForgotPasswordController {
 			model.addAttribute("error", "Something bad happened while sending the email.");
 		}
 		
-		return "forgottenPassord";
+		      return "forgottenPassord";
 		
 	}
 	
@@ -67,65 +62,57 @@ public class ForgotPasswordController {
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
 		
-		helper.setFrom("Geca@geca.com", "Java imitacija");
-		helper.setTo(email);
+		 helper.setFrom("Geca@geca.com", "Java imitacija");
+		 helper.setTo(email);
 		
 		String subject = "Someone triggered reset password for your account... ";
 		String content = "Here is the link to reset your password " +
 		                 "<a href=\"" + resetPasswordLink +"\"> Change my password</a>" 
 		                 +" if it wasnt you, you can ignore this email";
 		
-		helper.setSubject(subject);
-		helper.setText(content, true);
-		sender.send(message);
+		 helper.setSubject(subject);
+		 helper.setText(content, true);
+		   sender.send(message);
 		
 	}
 
 	public String getSiteUrl(HttpServletRequest request) {
 		String siteUrl = request.getRequestURL().toString();
-		return siteUrl.replace(request.getServletPath(), "");
+		     return siteUrl.replace(request.getServletPath(), "");
 	}
-	
 	
 	@GetMapping("/newPassword")
 	public String newPasswordForm(@Param(value = "token") String token, Model model) {
 		Users user = userService.getToken(token);
-		if(user == null) {
-			
+		 if(user == null) {
 			model.addAttribute("message", "Invalid Token");
-			return "message";
-			
+			 return "message";
 		}
-		
-		model.addAttribute("token", token);
-		
-		return "newPassword";
-		
+		    model.addAttribute("token", token);
+		     return "newPassword";
 	}
 	
 	@PostMapping("/newPassword")
 	public String setNewPassword(HttpServletRequest request,Model model) {
+		
 		String token = request.getParameter("token");
 		String password = request.getParameter("password");
-		
 		Users user = userService.getToken(token);
-              if(user == null) {
-            	  
-			   model.addAttribute("message", "Invalid Token");
-			return "message";
-			
-              }else {
-            	  
-            	  userService.updatePassword(user, password);
-            	  model.addAttribute("message", "You have succesfully changed your password and you can easily log in now...");
-      			
-              }
-		
-		
-              return "redirect:/login";
-		
-		
-		
+         if(user == null) {
+            model.addAttribute("message", "Invalid Token");
+			 return "message";
+			 
+	       } else {
+			 
+        try {
+		    userService.updatePassword(user, password);
+		} catch (InvalidPasswordException e) {
+			model.addAttribute("error", e.getMessage());
+			 return "newPassword";
+		}
+            model.addAttribute("message", "You have succesfully changed your password and you can easily log in now...");
+        }
+	         return "redirect:/login";
 	}
 	
 }
