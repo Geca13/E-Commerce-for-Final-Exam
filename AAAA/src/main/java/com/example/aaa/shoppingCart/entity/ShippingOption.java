7cc180ff074898004a1c56f323c0db2a16a16a30
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:16:59 +0100

    Update ShippingOption.java

--- AAAA/src/main/java/com/example/aaa/shoppingCart/entity/ShippingOption.java
+++ AAAA/src/main/java/com/example/aaa/shoppingCart/entity/ShippingOption.java
@@ -1,9 +1,10 @@
 package com.example.aaa.shoppingCart.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 public class ShippingOption {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String transporter;
