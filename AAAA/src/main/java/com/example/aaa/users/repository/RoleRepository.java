bb581013ceacb38126bf641053a1043d1d0630f3
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:39:48 +0100

    Update RoleRepository.java

--- AAAA/src/main/java/com/example/aaa/users/repository/RoleRepository.java
+++ AAAA/src/main/java/com/example/aaa/users/repository/RoleRepository.java
@@ -1,11 +1,7 @@
 package com.example.aaa.users.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.example.aaa.users.entity.Country;
 import com.example.aaa.users.entity.Role;
 import com.example.aaa.users.entity.RoleName;
 
