ef484f72ae95d2119923dc1044a0f757af7023a6
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-01-13 01:19:22 +0100

    Update CartProductsRepository.java

--- AAAA/src/main/java/com/example/aaa/shoppingCart/repository/CartProductsRepository.java
+++ AAAA/src/main/java/com/example/aaa/shoppingCart/repository/CartProductsRepository.java
@@ -1,7 +1,6 @@
 package com.example.aaa.shoppingCart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.aaa.shoppingCart.entity.CartProducts;
 
 public interface CartProductsRepository extends JpaRepository<CartProducts, Integer> {
